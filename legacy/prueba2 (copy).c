ec45dbf1b6318c7be0eabc5ac6ea62e17b3e2eda
Asier Galean Ullibarri <asiergalean@gmail.com>
2025-03-05 19:11:34 +0100

    norminette revisada y .h actualizado

--- legacy/prueba2 (copy).c
+++ legacy/prueba2 (copy).c
@@ -46,10 +46,12 @@ int	row_counter(char *route, t_info *info)
 		free(line);
 		line = get_next_line(fd);
 		rows++;
+		//printf("%d\n", rows);
 	}
 	close(fd);
 	free(line);
 	info->row_max = rows;
+	//printf("%d\n", info->row_max);
 	return (1);
 }
 
@@ -63,11 +65,14 @@ int	dump_map(char *route, t_info *info)
 
 	info->map = (char **)malloc(sizeof(char *) * (info->row_max + 1));
 	info->map[info->row_max] = 0;
+
 	fd = open(route, O_RDONLY);
+
 	i = 0;
 	while (i < info->row_max)
 	{
 		info->map[i] = get_next_line(fd);
+		//printf("%s", info->map[i]);
 		i++;
 	}
 	close(fd);
@@ -84,11 +89,14 @@ int	ff_dump_map(char *route, t_info *info)
 
 	info->ff_map = (char **)malloc(sizeof(char *) * (info->row_max + 1));
 	info->ff_map[info->row_max] = 0;
+
 	fd = open(route, O_RDONLY);
+
 	i = 0;
 	while (i < info->row_max)
 	{
 		info->ff_map[i] = get_next_line(fd);
+		//printf("%s", info->map[i]);
 		i++;
 	}
 	close(fd);
@@ -106,9 +114,11 @@ int	size_check(t_info *info)
 
 	if (!info->map)
 		return (0);
+
 	col_max = 0;
 	while (info->map[0][col_max])
 		col_max++;
+
 	row = 0;
 	while (info->map[row])
 	{
@@ -124,6 +134,7 @@ int	size_check(t_info *info)
 	info->col_max = col_max;
 	return (1);
 }
+
 
 //****************************************************************
 //****************************************************************
@@ -233,7 +244,7 @@ void	ff_map_transformer(t_info *info)
 //****************************************************************
 //****************************************************************
 
-void	floodfill(t_info *info, int row, int col)
+void floodfill(t_info *info, int row, int col)
 {
 	if (info->ff_map[row][col] == 'C')
 		info->ff_tokens++;
@@ -262,6 +273,7 @@ int	valid_path(t_info *info)
 	info->can_reach_exit = 0;
 	info->ff_tokens = 0;
 	floodfill(info, info->player_row, info->player_col);
+	
 	if (!info->can_reach_exit)
 		return (printf("Player can't reach exit\n"), 0);
 	if (info->ff_tokens != info->tokens)
@@ -272,11 +284,11 @@ int	valid_path(t_info *info)
 //****************************************************************
 //****************************************************************
 
-void	player_exit_pos_get(t_info *info)
+void player_exit_pos_get(t_info *info)
 {
 	int	i;
 	int	j;
-
+	
 	i = 0;
 	while (i < info->row_max)
 	{
@@ -334,7 +346,7 @@ int	is_good_map(t_info *info)
 void	ff_map_free(t_info *info)
 {
 	int	i;
-
+	
 	i = 0;
 	while (info->ff_map[i])
 	{
@@ -361,6 +373,7 @@ int	map_check(t_info *info)
 	}
 	ff_map_free(info);
 	return (1);
+
 }
 
 //****************************************************************
@@ -417,21 +430,21 @@ void	play_stats_init(t_info *info)
 void	load_map_sprites(t_img *img, void *mlx)
 {
 	img->floor = mlx_xpm_file_to_image(mlx, "sprites/ground.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->wall = mlx_xpm_file_to_image(mlx, "sprites/wall1.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->coin = mlx_xpm_file_to_image(mlx, "sprites/collectable.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->exit_0 = mlx_xpm_file_to_image(mlx, "sprites/exit_0.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->exit_25 = mlx_xpm_file_to_image(mlx, "sprites/exit_25.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->exit_50 = mlx_xpm_file_to_image(mlx, "sprites/exit_50.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->exit_75 = mlx_xpm_file_to_image(mlx, "sprites/exit_75.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->exit_100 = mlx_xpm_file_to_image(mlx, "sprites/exit_100.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 }
 
 //****************************************************************
@@ -440,23 +453,23 @@ void	load_map_sprites(t_img *img, void *mlx)
 void	load_player_sprites(t_img *img, void *mlx)
 {
 	img->p_idle = mlx_xpm_file_to_image(mlx, "sprites/gold_down_idle.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_up_1 = mlx_xpm_file_to_image(mlx, "sprites/gold_up1.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_up_2 = mlx_xpm_file_to_image(mlx, "sprites/gold_up2.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_down_1 = mlx_xpm_file_to_image(mlx, "sprites/gold_down1.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_down_2 = mlx_xpm_file_to_image(mlx, "sprites/gold_down2.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_left_1 = mlx_xpm_file_to_image(mlx, "sprites/gold_left.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_left_2 = mlx_xpm_file_to_image(mlx, "sprites/gold_left2.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_right_1 = mlx_xpm_file_to_image(mlx, "sprites/gold_right.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 	img->p_right_2 = mlx_xpm_file_to_image(mlx, "sprites/gold_right2.xpm",
-			&img->width, &img->height);
+		&img->width, &img->height);
 }
 
 //****************************************************************
@@ -471,11 +484,11 @@ void	load_all_sprites(t_img *img, void *mlx)
 //****************************************************************
 //****************************************************************
 
-void	map_to_window(void *mlx, void *mlx_win, t_info info, t_img img)
+void	map_to_window(void	*mlx, void *mlx_win,  t_info info, t_img img)
 {
 	int		i;
 	int		j;
-
+	
 	i = -1;
 	while (++i < info.row_max)
 	{
@@ -513,19 +526,19 @@ void	print_floor(t_info *info, t_window *mlx, t_img img)
 
 void	print_exit(t_info *info, t_window *mlx, t_img img)
 {
-	if ((info->owned_tokens * 100) / info->tokens < 25)
+	if ((info->owned_tokens * 100)/info->tokens < 25)
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, img.exit_0,
 			info->exit_col * 64, info->exit_row * 64);
-	else if ((info->owned_tokens * 100) / info->tokens < 50)
+	else if ((info->owned_tokens * 100)/info->tokens < 50)
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, img.exit_25,
 			info->exit_col * 64, info->exit_row * 64);
-	else if ((info->owned_tokens * 100) / info->tokens < 75)
+	else if ((info->owned_tokens * 100)/info->tokens < 75)
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, img.exit_50,
 			info->exit_col * 64, info->exit_row * 64);
-	else if ((info->owned_tokens * 100) / info->tokens < 100)
+	else if ((info->owned_tokens * 100)/info->tokens < 100)
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, img.exit_75,
 			info->exit_col * 64, info->exit_row * 64);
-	else if ((info->owned_tokens * 100) / info->tokens == 100)
+	else if ((info->owned_tokens * 100)/info->tokens == 100)
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, img.exit_100,
 			info->exit_col * 64, info->exit_row * 64);
 }
@@ -582,6 +595,7 @@ void	print_down(t_info *info, t_window *mlx, t_img img)
 			info->player_col * 64, info->player_row * 64);
 }
 
+
 //****************************************************************
 //****************************************************************
 
@@ -609,10 +623,10 @@ void	free_images(t_window *mlx)
 //****************************************************************
 //****************************************************************
 
-void	free_map(t_window *mlx)
+void free_map(t_window *mlx)
 {
 	int	i;
-
+	
 	i = 0;
 	while (mlx->info.map[i])
 	{
@@ -691,9 +705,9 @@ void	move_left(t_info *info, t_window *mlx)
 		info->player_col--;
 		printf("Number of moves: %d\n", ++info->move_count);
 	}
-	update_moving(info);
-	info->map[info->player_row][info->player_col] = 'P';
-	print_left(info, mlx, mlx->img);
+		update_moving(info);
+		info->map[info->player_row][info->player_col] = 'P';
+		print_left(info, mlx, mlx->img);
 }
 
 //****************************************************************
@@ -723,9 +737,9 @@ void	move_right(t_info *info, t_window *mlx)
 		info->player_col++;
 		printf("Number of moves: %d\n", ++info->move_count);
 	}
-	update_moving(info);
-	info->map[info->player_row][info->player_col] = 'P';
-	print_right(info, mlx, mlx->img);
+		update_moving(info);
+		info->map[info->player_row][info->player_col] = 'P';
+		print_right(info, mlx, mlx->img);
 }
 
 //****************************************************************
@@ -755,9 +769,9 @@ void	move_up(t_info *info, t_window *mlx)
 		info->player_row--;
 		printf("Number of moves: %d\n", ++info->move_count);
 	}
-	update_moving(info);
-	info->map[info->player_row][info->player_col] = 'P';
-	print_up(info, mlx, mlx->img);
+		update_moving(info);
+		info->map[info->player_row][info->player_col] = 'P';
+		print_up(info, mlx, mlx->img);
 }
 
 //****************************************************************
@@ -787,10 +801,11 @@ void	move_down(t_info *info, t_window *mlx)
 		info->player_row++;
 		printf("Number of moves: %d\n", ++info->move_count);
 	}
-	update_moving(info);
-	info->map[info->player_row][info->player_col] = 'P';
-	print_down(info, mlx, mlx->img);
+		update_moving(info);
+		info->map[info->player_row][info->player_col] = 'P';
+		print_down(info, mlx, mlx->img);
 }
+
 
 //****************************************************************
 //****************************************************************
@@ -807,32 +822,9 @@ int	key_hook(int keycode, t_window *mlx)
 		move_up(&mlx->info, mlx);
 	if (keycode == 115 || keycode == 65364)
 		move_down(&mlx->info, mlx);
+	//printf("Hello from key_hook! : %d\n", keycode);
+	
 	return (0);
-}
-
-//****************************************************************
-//****************************************************************
-
-void	map_and_ff_map_get(char *route, t_info *info)
-{
-	dump_map(route, info);
-	map_transformer(info);
-	ff_dump_map(route, info);
-	ff_map_transformer(info);
-}
-
-//****************************************************************
-//****************************************************************
-
-int	input_check(int arg_num, char *route)
-{
-	if (arg_num > 2)
-		return (printf("Too many arguments\n"), 0);
-	if (arg_num < 2)
-		return (printf("Too few arguments\n"), 0);
-	if (!extension_valid(route))
-		return (0);
-	return (1);
 }
 
 //****************************************************************
@@ -840,19 +832,42 @@ int	input_check(int arg_num, char *route)
 
 int	main(int argc, char *argv[])
 {
+	//t_info	info;
+	//int		i;
+	
+	
 	t_window	mlx;
+	//void	*mlx;
+	//void	*mlx_win;
+	
+	
+	//t_img	img;
 
-	if (!input_check(argc, argv[1]))
+	if (argc > 2)
+		return (printf("Too many arguments\n"), 1);
+	if (argc < 2)
+		return (printf("Too few arguments\n"), 1);
+	if (!extension_valid(argv[1]))
 		return (1);
-	if (!row_counter(argv[1], &mlx.info) || mlx.info.row_max < 3)
+	if(!row_counter(argv[1], &mlx.info) || mlx.info.row_max < 3)
 		return (printf("Invalid map\n"), 1);
-	map_and_ff_map_get(argv[1], &mlx.info);
+	dump_map(argv[1], &mlx.info);
+	map_transformer(&mlx.info);
+	
+	ff_dump_map(argv[1], &mlx.info);
+	ff_map_transformer(&mlx.info);
+	
+	//print_map(&mlx.info);
+	//printf("valid: %d\n", map_check(&mlx.info));
 	if (!map_check(&mlx.info))
 		return (free_map(&mlx), 1);
+	//printf("token count: %d\n", mlx.info.tokens);
+
 	play_stats_init(&mlx.info);
+	
 	mlx.mlx = mlx_init();
 	mlx.mlx_win = mlx_new_window(mlx.mlx,
-			mlx.info.col_max * 64, mlx.info.row_max * 64, "Pokemon Silver");
+		mlx.info.col_max * 64, mlx.info.row_max * 64, "Pokemon Silver");
 	load_all_sprites(&mlx.img, mlx.mlx);
 	map_to_window(mlx.mlx, mlx.mlx_win, mlx.info, mlx.img);
 	mlx_key_hook(mlx.mlx_win, key_hook, &mlx);
--- so_long.h
+++ so_long.h
@@ -22,24 +22,18 @@ typedef struct s_info
 	char	**map;
 	int		row_max;
 	int		col_max;
-	
 	int		tokens;
-	
 	int		ff_tokens;
 	int		can_reach_exit;
-
 	char	**ff_map;
-	
 	int		player_row;
 	int		player_col;
 	int		exit_row;
 	int		exit_col;
-	
 	int		owned_tokens;
 	int		move_count;
 	int		on_exit;
 	int		moving;
-	
 }		t_info;
 
 typedef struct s_img
@@ -49,8 +43,10 @@ typedef struct s_img
 	void	*p_up_2;
 	void	*p_down_1;
 	void	*p_down_2;
-	void	*p_left;
-	void	*p_right;
+	void	*p_left_1;
+	void	*p_left_2;
+	void	*p_right_1;
+	void	*p_right_2;
 	void	*floor;
 	void	*wall;
 	void	*coin;
@@ -61,10 +57,9 @@ typedef struct s_img
 	void	*exit_100;
 	int		width;
 	int		height;
-	
 }	t_img;
 
-typedef	struct s_window
+typedef struct s_window
 {
 	void	*mlx;
 	void	*mlx_win;
@@ -74,7 +69,48 @@ typedef	struct s_window
 	int		img_height;
 	int		win_width;
 	int		win_height;
-	
 }		t_window;
 
-
+int		extension_valid(char *file_name);
+int		fd_check(int fd);
+int		row_counter(char *route, t_info *info);
+int		dump_map(char *route, t_info *info);
+int		ff_dump_map(char *route, t_info *info);
+int		size_check(t_info *info);
+int		check_borders(t_info *info);
+int		check_tiles(t_info *info);
+int		player_exit_number(t_info *info);
+void	ff_map_transformer(t_info *info);
+void	floodfill(t_info *info, int row, int col);
+int		valid_path(t_info *info);
+void	player_exit_pos_get(t_info *info);
+int		check_objects(t_info *info);
+int		is_good_map(t_info *info);
+void	ff_map_free(t_info *info);
+int		map_check(t_info *info);
+void	map_transformer(t_info *info);
+void	print_map(t_info *info);
+void	play_stats_init(t_info *info);
+void	load_map_sprites(t_img *img, void *mlx);
+void	load_player_sprites(t_img *img, void *mlx);
+void	load_all_sprites(t_img *img, void *mlx);
+void	map_to_window(void *mlx, void *mlx_win, t_info info, t_img img);
+void	print_floor(t_info *info, t_window *mlx, t_img img);
+void	print_exit(t_info *info, t_window *mlx, t_img img);
+void	print_left(t_info *info, t_window *mlx, t_img img);
+void	print_right(t_info *info, t_window *mlx, t_img img);
+void	print_up(t_info *info, t_window *mlx, t_img img);
+void	print_down(t_info *info, t_window *mlx, t_img img);
+void	free_images(t_window *mlx);
+void	free_map(t_window *mlx);
+int		close_game(t_window *mlx);
+void	exit_position_update_and_print(t_info *info, t_window *mlx);
+void	floor_position_update_and_print(t_info *info, t_window *mlx);
+void	update_moving(t_info *info);
+void	move_left(t_info *info, t_window *mlx);
+void	move_right(t_info *info, t_window *mlx);
+void	move_up(t_info *info, t_window *mlx);
+void	move_down(t_info *info, t_window *mlx);
+int		key_hook(int keycode, t_window *mlx);
+void	map_and_ff_map_get(char *route, t_info *info);
+int		input_check(int arg_num, char *route);
